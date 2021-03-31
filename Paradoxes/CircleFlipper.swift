9f4ec888d2f53e25a6b0e67eb21a016992d0b919
leTarrask <contato@tarrask.com>
2021-03-31 10:32:35 +0100

    cleaned code & made half the calculation

--- Paradoxes/CircleFlipper.swift
+++ Paradoxes/CircleFlipper.swift
@@ -29,61 +29,90 @@ struct CircleFlipper: View {
             .repeatForever()
     }
 
-    var radius: CGFloat = 120
+    var radius: CGFloat
+
+    init(radius: CGFloat) {
+        self.radius = radius
+        x0 = radius * cos(0)
+        y0 = radius * sin(0)
+        x1 = radius * cos(1*multiplier)
+        y1 = radius * sin(1*multiplier)
+        x2 = radius * cos(2*multiplier)
+        y2 = radius * sin(2*multiplier)
+        x3 = radius * cos(3*multiplier)
+        y3 = radius * sin(3*multiplier)
+        x4 = radius * cos(4*multiplier)
+        y4 = radius * sin(4*multiplier)
+        x5 = radius * cos(5*multiplier)
+        y5 = radius * sin(5*multiplier)
+        x6 = radius * cos(6*multiplier)
+        y6 = radius * sin(6*multiplier)
+        x7 = radius * cos(7*multiplier)
+        y7 = radius * sin(7*multiplier)
+        x8 = radius * cos(8*multiplier)
+        y8 = radius * sin(8*multiplier)
+        x9 = radius * cos(9*multiplier)
+        y9 = radius * sin(9*multiplier)
+        x10 = radius * cos(10*multiplier)
+        y10 = radius * sin(10*multiplier)
+        x11 = radius * cos(11*multiplier)
+        y11 = radius * sin(11*multiplier)
+    }
+
     let multiplier = CGFloat.pi/6
     
     var body: some View {
         ZStack {
             Group {
                 Circulito(color: .pink).opacity(0.5)
-                    .offset(x: fireOne ? radius * cos(0) : radius * cos(6*multiplier),
-                            y: fireOne ? radius * sin(0) : radius * sin(6*multiplier))
+                    .offset(x: fireOne ? x0 : x6,
+                            y: fireOne ? y0 : y6)
 
                 Circulito(color: .red)
-                    .offset(x: fireTwo ? radius * cos(1*multiplier) : radius * cos(7*multiplier),
-                            y: fireTwo ? radius * sin(1*multiplier) : radius * sin(7*multiplier))
+                    .offset(x: fireTwo ? x1 : x7,
+                            y: fireTwo ? y1 : y7)
 
                 Circulito(color: .orange)
-                    .offset(x: fireThree ? radius * cos(2*multiplier) : radius * cos(8*multiplier),
-                            y: fireThree ? radius * sin(2*multiplier) : radius * sin(8*multiplier))
+                    .offset(x: fireThree ? x2 : x8,
+                            y: fireThree ? y2 : y8)
 
                 Circulito(color: Color.yellow.opacity(0.5))
-                    .offset(x: fireFour ? radius * cos(3*multiplier) : radius * cos(9*multiplier),
-                            y: fireFour ? radius * sin(3*multiplier) : radius * sin(9*multiplier))
+                    .offset(x: fireFour ? x3 : x9,
+                            y: fireFour ? y3 : y9)
 
                 Circulito(color: .yellow)
-                    .offset(x: fireFive ? radius * cos(4*multiplier) : radius * cos(10*multiplier),
-                            y: fireFive ? radius * sin(4*multiplier) : radius * sin(10*multiplier))
+                    .offset(x: fireFive ? x4 : x10,
+                            y: fireFive ? y4 : y10)
 
                 Circulito(color: Color.green.opacity(0.5))
-                    .offset(x: fireSeven ? radius * cos(5*multiplier) : radius * cos(11*multiplier),
-                            y: fireSeven ? radius * sin(5*multiplier) : radius * sin(11*multiplier))
+                    .offset(x: fireSeven ? x5 : x11,
+                            y: fireSeven ? y5 : y11)
             }
 
             Group {
                 Circulito(color: .green)
-                    .offset(x: fireSix ? radius * cos(6*multiplier) : radius * cos(0),
-                            y: fireSix ? radius * sin(6*multiplier) : radius * sin(0))
+                    .offset(x: fireSix ? x6 : x0,
+                            y: fireSix ? y6 : y0)
 
                 Circulito(color: Color.blue.opacity(0.5))
-                    .offset(x: fireEight ? radius * cos(7*multiplier) : radius * cos(1*multiplier),
-                            y: fireEight ? radius * sin(7*multiplier) : radius * sin(1*multiplier))
+                    .offset(x: fireEight ? x7 : x1,
+                            y: fireEight ? y7 : y1)
 
                 Circulito(color: .blue)
-                    .offset(x: fireNine ? radius * cos(8*multiplier) : radius * cos(2*multiplier),
-                            y: fireNine ? radius * sin(8*multiplier) : radius * sin(2*multiplier))
+                    .offset(x: fireNine ? x8 : x2,
+                            y: fireNine ? y8 : y2)
 
                 Circulito(color: .purple)
-                    .offset(x: fireTen ? radius * cos(9*multiplier) : radius * cos(3*multiplier),
-                            y: fireTen ? radius * sin(9*multiplier) : radius * sin(3*multiplier))
+                    .offset(x: fireTen ? x9 : x3,
+                            y: fireTen ? y9 : y3)
 
                 Circulito(color: .gray)
-                    .offset(x: fireEleven ? radius * cos(10*multiplier) : radius * cos(4*multiplier),
-                            y: fireEleven ? radius * sin(10*multiplier) : radius * sin(4*multiplier))
+                    .offset(x: fireEleven ? x10 : x4,
+                            y: fireEleven ? y10 : y4)
 
                 Circulito(color: .black)
-                    .offset(x: fireTwelve ? radius * cos(11*multiplier) : radius * cos(5*multiplier),
-                            y: fireTwelve ? radius * sin(11*multiplier) : radius * sin(5*multiplier))
+                    .offset(x: fireTwelve ? x11 : x5,
+                            y: fireTwelve ? y11 : y5)
             }
         }
         .frame(width: 1, height: 1, alignment: .center)
@@ -126,11 +155,37 @@ struct CircleFlipper: View {
             }
         }
     }
+
+    // Ball positions
+    var x0: CGFloat
+    var y0: CGFloat
+    var x1: CGFloat
+    var y1: CGFloat
+    var x2: CGFloat
+    var y2: CGFloat
+    var x3: CGFloat
+    var y3: CGFloat
+    var x4: CGFloat
+    var y4: CGFloat
+    var x5: CGFloat
+    var y5: CGFloat
+    var x6: CGFloat
+    var y6: CGFloat
+    var x7: CGFloat
+    var y7: CGFloat
+    var x8: CGFloat
+    var y8: CGFloat
+    var x9: CGFloat
+    var y9: CGFloat
+    var x10: CGFloat
+    var y10: CGFloat
+    var x11: CGFloat
+    var y11: CGFloat
 }
 
 struct CircleFlipper_Previews: PreviewProvider {
     static var previews: some View {
-        CircleFlipper()
+        CircleFlipper(radius: 120.0)
             .previewDevice("iPhone 8")
             .previewLayout(.device)
     }
